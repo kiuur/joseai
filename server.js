21957a7daf265eb05ebfbf316cf8332a183ff22b
Sh3nn <kyuurzy@gmail.com>
2025-08-04 16:28:32 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fetch = require('node-fetch');
-
+const axios = require('axios')
 const app = express();
 const PORT = process.env.PORT || 5000;
 
